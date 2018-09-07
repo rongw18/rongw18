2bcdee196626bd7145973384b004ed0799eca767
rongw18 <42684227+rongw18@users.noreply.github.com>
2018-09-07 15:15:36 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -6,6 +6,14 @@
 //  Copyright © 2018年 Aurora. All rights reserved.
 //
 #include <stdio.h>
+#include <string.h>
+/**
+ * Parses pattern for next character and sets
+ * necessary parameters that are referenced inside/outside
+ * of this function.
+ *
+ * Add any additional inputs to function signature if necessary.
+ */
 #define MAXSIZE 4096
 
 /**
@@ -16,59 +24,64 @@
 
 
 
-int matches_leading(char *partial_line, char *pattern) {
-    // Implement if desire
-    //
+int matches_leading(char *partial_line, char *pattern, char *ori) {
     if (*partial_line != '\0') {
         if (*pattern != '\0') {
             // backslash
             if (*pattern == '\\' && *(pattern-1) != '\\') {
                 //Back slash will skip a spot in pattern如果第二个\前面还有一个\，第二个\会被略过
                 pattern++;
-                return matches_leading(partial_line,pattern);
-            }
+                return matches_leading(partial_line,pattern, ori);
+            }  // \ends
             // ?
             if (*(pattern+1) == '?' && *(pattern-1) != '\\') {
                 if(*pattern == '.'){
                     if (*(partial_line) == *(pattern+2)){
-                        if(*(pattern+2) == *(partial_line+1)){
+                        if(*(pattern+2) == *(partial_line+1)){ //youdianwenti
+                            pattern++;
                             pattern++;
                             partial_line++;
                         }
-                        else{
+                        else{   //meiwenti
                             pattern++;
                             pattern++;
                         }
                     }
-                else{
+                    else{    // *(partial_line) != *(pattern+2) //youwenti
                         pattern++;
                         pattern++;
                         partial_line++;
                     }
-                
-                    //Check if code reaches the end of hthe sentence, where character is '0'
-                    if(matches_leading(partial_line,pattern))//if
-                        return 1;
-                    else
-                        return 0;
                 }
-            
-                if (*pattern == '.'){//如果两个if都满足呢？
+                else{   //?qianmianmeiyou.
+                    if(*pattern != *partial_line){
+                        pattern++;
+                        pattern++;
+                    }
+                    else{
+                        pattern++;
+                        partial_line++;
+                    }
+                    }
+                    //Check if code reaches the end of hthe sentence, where character is '0'
+                while(matches_leading(partial_line,pattern,ori)){//if
+                    return 1;
+                }
+                while(!matches_leading(partial_line,pattern,ori)){
+                    return 0;
+                }
+            }  //? ends
+        
+              /*  if (*pattern == '.'){//如果两个if都满足呢？
                     if( matches_leading(partial_line,pattern+2)) // if the current letter fits in如果和+2位相同的话，即？前省略.代表的字符
                         return 1;
                     if( matches_leading(partial_line+1,pattern+2))// or if the next letter fits in j即？前没省略.
                         return 1;
                     else
                         return 0;
-                }
-                if (*pattern == *(pattern+2)){ //If hi?h?hi, then it will be equivalent to hhi, hihhi, etc.这个是不是应该删掉
-                    pattern+=2;
-                    return matches_leading(partial_line,pattern);
-                }
-                if (*pattern == *partial_line){
-                    return matches_leading(partial_line+1,pattern+2);
-                }
-            }
+                } */
+            
+            
             // +
             /*
              if (*pattern == '+'){
@@ -86,53 +99,71 @@ int matches_leading(char *partial_line, char *pattern) {
              */
             
             if (*(pattern+1) == '+' && *(pattern-1) != '\\') { //The character before this index will appear one or more times in a row
-                int i = 0;
-                int j = 1;
                 if (*pattern == '.'){
-                    while(*(pattern+2) != *partial_line) {//每次都加到头，再从不一样的地方开始接着比较
+                     if(*partial_line== *(partial_line+1)){
+                         char temp=*partial_line;
+                         while(*partial_line ==*(partial_line+1)){
+                             partial_line++;
+                         }
+                         pattern++;
+                         pattern++;
+                         while(*pattern == temp){
+                             pattern++;
+                         }
+                     }
+                     else{
+                         pattern++;
+                         pattern++;
+                         partial_line++;
+                     }
+                 }
+                else{  // actually they are the same
+                    if(*partial_line== *(partial_line+1)){
+                        char tempp=*partial_line;
+                        while(*partial_line ==*(partial_line+1)){
+                            partial_line++;
+                        }
+                        pattern++;
+                        pattern++;
+                        while(*pattern == tempp){
+                            pattern++;
+                        }
+                    }
+                    else{
+                        pattern++;
+                        pattern++;
                         partial_line++;
                     }
-                    partial_line--;
-                    pattern++;
-                    pattern++;
-                    while(*pattern == *partial_line){
-                        pattern++;
-                    }
-                    pattern--;
-                    if(*partial_line == '\0'){
-                            return 0;
-                    }
-                    return matches_leading(partial_line,pattern);
                 }
-                while (partial_line[i] == *pattern) { //While the partial lin
-                    i++;
-                }
-                while (j <= i) {
-                    j++;
-                    if (pattern+j == NULL)
-                        return matches_leading(partial_line+i,pattern+j);
-                    if(pattern[j] != *pattern)
-                        break;
-                }
-                return matches_leading(partial_line+i,pattern+j);
             }
+                     
+                     
+                     
+               
             
             // CHARACTER MATCH
             if (*pattern == *partial_line) {
-                return matches_leading(partial_line+1,pattern+1); //increment both pattern and partial line if they have the same character, ie they match
+                return matches_leading(partial_line+1,pattern+1,ori); //increment both pattern and partial line if they have the same character, ie they match
             }
             // .
             if (*pattern == '.' && *(pattern-1) != '\\') {
-                return matches_leading(partial_line+1,pattern+1); //go on to the next two
+                return matches_leading(partial_line+1,pattern+1,ori); //go on to the next two
             }
             // NO MATCH NO SPECIAL CHARACTER
             else {
-                return 0;
+                *pattern = *ori;
+                 return matches_leading(partial_line+1,pattern,ori);
             }
+             return 1;
         }
+        else{
         return 1;
+        }
     }
-    return 0;
+    else if(*partial_line == '\0' && *pattern == '\0'){
+    return 1;
+    }
+    else return 0;
     
 }
 
@@ -151,7 +182,7 @@ int rgrep_matches(char *line, char *pattern) {
         return 1;
     int k = 0;
     while (line[k] != '\0') {
-        if (matches_leading(line+k, pattern))
+        if (matches_leading(line+k, pattern,pattern))
             return 1;
         k++;
     }
@@ -161,31 +192,24 @@ int rgrep_matches(char *line, char *pattern) {
     return 0;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <PATTERN>\n", argv[0]);
-        return 2;
-    }
-    
-    /* we're not going to worry about long lines */
-    char buf[MAXSIZE];
-    
-    while (!feof(stdin) && !ferror(stdin)) {
-        if (!fgets(buf, sizeof(buf), stdin)) {
+
+
+int main() {
+    char a[100],b[100];
+    printf("Enter string you want to search:\n");
+    gets(a);
+    printf("THEN enter the pattern you search for:\n");
+    gets(b);
+    switch (matches_leading(a, b,b)) {
+        case 0:
+            printf("No such a pattern\n");
             break;
-        }
-        if (rgrep_matches(buf, argv[1])) {
-            fputs(buf, stdout);
-            fflush(stdout);
-        }
+        case 1:
+            printf("Yes! We got it!\n");
+            break;
+            
+        default:
+            break;
     }
-    
-    if (ferror(stdin)) {
-        perror(argv[0]);
-        return 1;
-    }
-    
     return 0;
 }
-
-
